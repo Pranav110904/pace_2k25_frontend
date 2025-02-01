6f7911b3b1f1e146becaba17acdc1c2b7e93d214
Pranav110904 <151397720+Pranav110904@users.noreply.github.com>
2025-02-01 12:26:29 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import OurTeam from './pages/OurTeam';
 
 function App() {
   return (
     <>
       <Navbar /> 
-      <div className=''>App</div>
-      <div ></div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/our-team" element={<OurTeam />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
