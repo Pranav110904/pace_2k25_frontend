107cb666de912ac3b150778679f0afd8e88d769a
SHARMA-ANK <ankitsharma_22248@aitpunr.edu.in>
2025-02-02 15:53:25 +0530

    Add Event Slider and the Sponser Form

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Sponsors from "../components/Sponsor";
+import SponsorForm from "../components/SponsorForm";
+import EventSlider from "../components/EventSlider";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <About />
+      <EventSlider/>
       <Sponsors />
+      <SponsorForm/>
     </>
   );
 };
